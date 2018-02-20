48222d6d1fa9723504c9e9b85d1701474c2f155d
Samuel Gruetter <samuel.gruetter@epfl.ch>
2018-02-20 15:44:14 -0500

    simplify decode_encode (let pulling not needed) and profile
    it takes 33min and 2.7GB, not sure how to optimize further
    
     tactic                                   local  total   calls       max
    ────────────────────────────────────────┴──────┴──────┴───────┴─────────┘
    ─apply simpl_dec_and_neq ---------------  27.8%  27.8%    1964    3.445s
    ─rewrite <- E --------------------------  27.8%  27.8%     474    1.161s
    ─subst (ne_var_list) -------------------  20.9%  20.9%    1479    1.530s
    ─destruct H as [H' H] ------------------  12.3%  12.3%     321    2.511s
    ─apply simpl_dec_and_neq_2 -------------   2.9%   2.9%     699    0.230s
    ─apply simpl_dec_final_neq -------------   2.8%   2.8%     513    0.389s
    ─apply simpl_dec_and_eq ----------------   2.2%   2.2%     347    0.214s

--- src/DecodeEncode.v
+++ src/DecodeEncode.v
@@ -92,77 +92,6 @@ Lemma invert_encode_UJ(opcode: word 7)(rd: word 5)(jimm20: Z)(inst: word 32):
                                 shift (bitSlice inst 12 20) 12).
 Proof. Admitted.
 
-(*
-Hint Unfold encode_R encode_I encode_I_shift encode_I_system encode_S encode_SB encode_U encode_UJ :
-  unfold_encode_helpers.
-
-Lemma Some_inj: forall {A: Type} (a1 a2: A), Some a1 = Some a2 -> a1 = a2.
-Proof. intros. inversion H. reflexivity. Qed.
-*)
-
-Lemma pull_let: forall {A B: Type} (a: A) (b: A -> B) (c: B),
-  let x := a in (b x = c) ->
-  (let x := a in b x) = c.
-Proof. intros. simpl. subst x. assumption. Qed.
-
-(*
-Lemma get_opcode_I: forall (opcode: word 7) (funct3: word 3) (rd rs1: word 5) (oimm12: word 12),
-  opcode = bitSlice' (combine (combine (combine (combine
-              opcode rd) funct3) rs1) oimm12) 0 7.
-Proof.
-  intros. unfold bitSlice'. simpl. unfold split_middle, split_upper, split_lower.
-  unfold id.
-Admitted.
-
-Lemma get_funct3_I: forall (opcode: word 7) (funct3: word 3) (rd rs1: word 5) (oimm12: word 12),
-  funct3 = bitSlice' (combine (combine (combine (combine
-              opcode rd) funct3) rs1) oimm12) 12 15.
-Proof.
-  intros. unfold bitSlice'. simpl. unfold split_middle, split_upper, split_lower.
-  unfold id.
-Admitted.
-*)
-
-  (*
-Lemma decode_encode: forall (inst: Instruction) (w: word 32),
-  encode inst = Some w ->
-  decode w = inst.
-Proof.
-  intros. destruct inst; simpl in *; try discriminate;
-  autounfold with unfold_encode_helpers in *;
-  lazymatch goal with
-  | H: context [ @dec ?P ?D ] |- _ => destruct (@dec P D); [|discriminate]
-  | _ => idtac
-  end;
-  apply Some_inj in H;
-  subst w.
-  let d := eval cbv delta [decode] in decode in change decode with d.
-  cbv beta.
-- repeat match goal with
-  | |- (let x := ?a in ?b) = ?c => cut (let x := a in (b = c));
-       [apply (pull_let a (fun x => b) c) | intro]
-  end.
-  Admitted.
-  evar (oc: word 7).
-  replace opcode with oc by (subst oc opcode; apply get_opcode_I).
-  subst oc opcode.
-  evar (f3: word 3).
-  replace funct3 with f3 by (subst f3 funct3; apply get_funct3_I).
-  subst f3 funct3.
-  lazymatch goal with
-  | |- (if ?A then ?B else ?C) = ?D =>
-     let r := eval hnf in A in
-     change ((if r then B else C) = D);
-     cbv beta iota
-  end.
-  f_equal; clear -a.
-  + admit.
-  + admit.
-  + admit.
--
-Admitted.
-*)
-
 Lemma simpl_dec_and_eq: forall (A: Type) (a: A) (P: Prop) (T: Type) (e1 e2 e3: T) da dP,
   (if @dec P dP then e1 else e2) = e3 ->
   (if @dec_and (a = a) P da dP then e1 else e2) = e3.
@@ -213,20 +142,15 @@ Ltac deep_destruct_and H :=
   | _ => idtac
   end.
 
+Set Ltac Profiling.
+
 Lemma decode_encode: forall (inst: Instruction) (w: word 32),
   encode inst = Some w ->
   decode w = inst.
 Proof.
   intros.
   let d := eval cbv delta [decode] in decode in change decode with d.
-  cbv beta.
-  repeat match goal with
-  | |- (let x := ?a in ?b) = ?c => cut (let x := a in (b = c));
-       [apply (pull_let a (fun x => b) c) | intro]
-  end.
-  repeat match goal with
-    | v: if _ then _ else _ |- _ => progress compute in (type of v)
-  end.
+  cbv beta zeta.
   destruct inst;
   let force_evaluation_order_dummy := constr:(0) in time (
   tryif (solve [ abstract (
@@ -271,3 +195,5 @@ Proof.
     end
   )).
 Time Qed.
+
+Show Ltac Profile.
