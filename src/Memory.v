3721e3690db843a22f50a81aa536eb8d4d75fb8f
Samuel Gruetter <samuel.gruetter@epfl.ch>
2018-03-02 22:14:52 -0500

    memory interface like hs, but address is param of class, and option

--- src/Memory.v
+++ src/Memory.v
@@ -2,6 +2,18 @@ Require Import bbv.Word.
 Require Import Coq.ZArith.BinInt.
 Require Import riscv.util.Decidable.
 Require Import Coq.Lists.List.
+Require Import riscv.Utility.
+
+Class Memory(m: Set)(a: Set) := mkMemory {
+  loadByte : m -> a -> option Word8;
+  loadHalf : m -> a -> option Word16;
+  loadWord : m -> a -> option Word32;
+  loadDouble : m -> a -> option Word64;
+  storeByte : m -> a -> option Word8 -> m;
+  storeHalf : m -> a -> option Word16 -> m;
+  storeWord : m -> a -> option Word32 -> m;
+  storeDouble : m -> a -> option Word64 -> m;
+}.
 
 (* memory addresses are represented using Z because word does not restrict them enough,
    there are always invalid memory addresses, which are represented by returning None,
