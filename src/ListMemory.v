f0dc61e063d1927a4b34b9f02f2439be07217da3
Samuel Gruetter <samuel.gruetter@epfl.ch>
2018-05-04 10:55:12 -0400

    with valid_addr, the sigma type is not needed any more
    a + alignment <= length
    
    ensures non-empty because alignment is at least 1, and doing
    comparisions in nat instead of word prevents overflows

--- src/ListMemory.v
+++ src/ListMemory.v
@@ -9,90 +9,46 @@ Require Import riscv.util.Monads.
 Require Import riscv.Memory.
 Require riscv.ListMemoryNatAddr.
 
-Lemma mem_size_write_byte_non_zero: forall m a v,
-    ListMemoryNatAddr.mem_size (ListMemoryNatAddr.write_byte m a v) > 0.
-Proof.
-  intros.
-  unfold ListMemoryNatAddr.write_byte. unfold ListMemoryNatAddr.mem_size.
-  rewrite? app_length. simpl. omega.
-Qed.
-
-Lemma mem_size_write_half_non_zero: forall m a v,
-    ListMemoryNatAddr.mem_size (ListMemoryNatAddr.write_half m a v) > 0.
-Proof.
-  intros.
-  unfold ListMemoryNatAddr.write_half. unfold ListMemoryNatAddr.mem_size.
-  unfold ListMemoryNatAddr.write_byte.
-  rewrite? app_length. simpl. omega.
-Qed.
-
-Lemma mem_size_write_word_non_zero: forall m a v,
-    ListMemoryNatAddr.mem_size (ListMemoryNatAddr.write_word m a v) > 0.
-Proof.
-  intros.
-  unfold ListMemoryNatAddr.write_word. unfold ListMemoryNatAddr.mem_size.
-  unfold ListMemoryNatAddr.write_half. unfold ListMemoryNatAddr.mem_size.
-  unfold ListMemoryNatAddr.write_byte.
-  rewrite? app_length. simpl. omega.
-Qed.
-
-Lemma mem_size_write_double_non_zero: forall m a v,
-    ListMemoryNatAddr.mem_size (ListMemoryNatAddr.write_double m a v) > 0.
-Proof.
-  intros.
-  unfold ListMemoryNatAddr.write_double. unfold ListMemoryNatAddr.mem_size.
-  unfold ListMemoryNatAddr.write_word. unfold ListMemoryNatAddr.mem_size.
-  unfold ListMemoryNatAddr.write_half. unfold ListMemoryNatAddr.mem_size.
-  unfold ListMemoryNatAddr.write_byte.
-  rewrite? app_length. simpl. omega.
-Qed.
 
 Section Memory.
 
   Context {w: nat}. (* bit width of memory addresses *)
 
-  Definition mem := { m: ListMemoryNatAddr.mem | ListMemoryNatAddr.mem_size m > 0 }.
-  Definition max_addr(m: mem): word w :=
-    natToWord w (pred (ListMemoryNatAddr.mem_size (proj1_sig m))).
+  Definition mem := ListMemoryNatAddr.mem.
+  Definition mem_size: mem -> nat := ListMemoryNatAddr.mem_size.
 
   Definition read_byte(m: mem)(a: word w): word 8 :=
-    ListMemoryNatAddr.read_byte (proj1_sig m) (wordToNat a).
+    ListMemoryNatAddr.read_byte m (wordToNat a).
 
   Definition read_half(m: mem)(a: word w): word 16 :=
-    ListMemoryNatAddr.read_half (proj1_sig m) (wordToNat a).
+    ListMemoryNatAddr.read_half m (wordToNat a).
 
   Definition read_word(m: mem)(a: word w): word 32 :=
-    ListMemoryNatAddr.read_word (proj1_sig m) (wordToNat a).
+    ListMemoryNatAddr.read_word m (wordToNat a).
 
   Definition read_double(m: mem)(a: word w): word 64 :=
-    ListMemoryNatAddr.read_double (proj1_sig m) (wordToNat a).
+    ListMemoryNatAddr.read_double m (wordToNat a).
 
-  Definition write_byte(m: mem)(a: word w)(v: word 8): mem.
-    refine (exist _ (ListMemoryNatAddr.write_byte (proj1_sig m) (wordToNat a) v) _).
-    apply mem_size_write_byte_non_zero.
-  Defined.
+  Definition write_byte(m: mem)(a: word w)(v: word 8): mem :=
+    ListMemoryNatAddr.write_byte m (wordToNat a) v.
 
-  Definition write_half(m: mem)(a: word w)(v: word 16): mem.
-    refine (exist _ (ListMemoryNatAddr.write_half (proj1_sig m) (wordToNat a) v) _).
-    apply mem_size_write_half_non_zero.
-  Defined.
+  Definition write_half(m: mem)(a: word w)(v: word 16): mem :=
+    ListMemoryNatAddr.write_half m (wordToNat a) v.
 
-  Definition write_word(m: mem)(a: word w)(v: word 32): mem.
-    refine (exist _ (ListMemoryNatAddr.write_word (proj1_sig m) (wordToNat a) v) _).
-    apply mem_size_write_word_non_zero.
-  Defined.
+  Definition write_word(m: mem)(a: word w)(v: word 32): mem :=
+    ListMemoryNatAddr.write_word m (wordToNat a) v.
 
-  Definition write_double(m: mem)(a: word w)(v: word 64): mem.
-    refine (exist _ (ListMemoryNatAddr.write_double (proj1_sig m) (wordToNat a) v) _).
-    apply mem_size_write_double_non_zero.
-  Defined.
+  Definition write_double(m: mem)(a: word w)(v: word 64): mem :=
+    ListMemoryNatAddr.write_double m (wordToNat a) v.
 
-  Definition const_mem(default: word 8)(max_addr: nat): mem.
-    refine (exist _ (ListMemoryNatAddr.const_mem default (S max_addr)) _).
-    simpl. apply gt_Sn_O.
-  Defined.
+  Definition const_mem(default: word 8)(max_addr: nat): mem :=
+    ListMemoryNatAddr.const_mem default (S max_addr).
 
   Definition zero_mem: nat -> mem := const_mem $0.
+
+  Definition valid_addr(m: mem)(alignment: nat)(addr: word w): Prop :=
+    wordToNat addr + alignment <= mem_size m /\
+    (wordToNat addr) mod alignment = 0.
 
 End Memory.
 
@@ -108,109 +64,56 @@ Instance mem_is_Memory(w: nat): Memory mem (word w) := {|
 |}.
 
 Lemma write_read_byte_eq: forall w m (a1 a2: word w) v,
-    (a1 <= (max_addr m))%word ->
+    valid_addr m 1 a1 ->
     a2 = a1 ->
     read_byte (write_byte m a1 v) a2 = v.
 Proof.
-  intros.
-  unfold max_addr, ListMemoryNatAddr.mem_size in *.
-  destruct m as [m P]. simpl in *. unfold ListMemoryNatAddr.mem_size in *.
+  intros. destruct H.
+  unfold mem_size, ListMemoryNatAddr.mem_size in *.
+  unfold ListMemoryNatAddr.mem_size in *.
   apply ListMemoryNatAddr.write_read_byte_eq.
   - simpl.
-    destruct m; simpl in *; [omega|]. unfold ListMemoryNatAddr.mem_size.
-    apply le_lt_n_Sm.
-    apply le_word_le_nat.
-    assumption.
+    unfold ListMemoryNatAddr.mem_size.
+    omega.
   - f_equal. assumption.
 Qed.
 
 Lemma write_read_byte_ne: forall w m (a1 a2: word w) v,
-    (a1 <= (max_addr m))%word ->
-    (a2 <= (max_addr m))%word ->
+    valid_addr m 1 a1 ->
+    valid_addr m 1 a2 ->
     a2 <> a1 ->
     read_byte (write_byte m a1 v) a2 = read_byte m a2.
 Proof.
   intros.
-  unfold max_addr, ListMemoryNatAddr.mem_size in *.
-  destruct m as [m P]. simpl in *. unfold ListMemoryNatAddr.mem_size in *.
-  destruct m; simpl in *; [omega|].
+  destruct H, H0.
+  unfold mem_size, ListMemoryNatAddr.mem_size in *.
   apply ListMemoryNatAddr.write_read_byte_ne; simpl; unfold ListMemoryNatAddr.mem_size.
-  - apply le_lt_n_Sm.
-    apply le_word_le_nat.
-    assumption.
-  - apply le_lt_n_Sm.
-    apply le_word_le_nat.
-    assumption.
-  - intro C. apply wordToNat_inj in C. contradiction.
+  - omega.
+  - omega.
+  - apply (wordToNat_neq1 H1).
 Qed.
 
-Lemma write_byte_preserves_max_addr: forall w m (a: word w) v,
-    (a <= (max_addr m))%word ->
-    @max_addr w (write_byte m a v) = @max_addr w m.
+Lemma write_byte_preserves_mem_size: forall w m (a: word w) v,
+    valid_addr m 1 a ->
+    mem_size (write_byte m a v) = mem_size m.
 Proof.
-  intros.
-  unfold max_addr, write_byte in *.
-  do 2 f_equal.
+  intros. destruct H.
+  unfold mem_size, write_byte in *.
   apply ListMemoryNatAddr.write_byte_preserves_mem_size.
-  destruct m as [m P]. simpl in *. unfold ListMemoryNatAddr.mem_size in *.
-  destruct m; simpl in *; [omega|].
-  apply le_lt_n_Sm.
-  apply le_word_le_nat.
-  assumption.
+  omega.
 Qed.
-
-Lemma write_read_half_eq: forall w m (a1 a2: word w) v,
-    (a1 <= max_addr m ^- $1)%word ->
-    a2 = a1 ->
-    read_half (write_half m a1 v) a2 = v.
-Proof.
-  intros.
-  destruct m as [m P]. unfold ListMemoryNatAddr.mem_size in *.
-  apply ListMemoryNatAddr.write_read_half_eq.
-  - simpl.
-    destruct m; simpl in *; [omega|]. unfold ListMemoryNatAddr.mem_size.
-    apply le_lt_n_Sm.
-    unfold max_addr, ListMemoryNatAddr.mem_size in H.
-    simpl in H.
-    unfold wminus in H.
-    (* minus is a bad idea because bigger minuses might run below 0 *)
-Abort.
-
-Lemma write_read_half_eq: forall w m (a1 a2: word w) v,
-    (a1 ^+ $1 <= max_addr m)%word ->
-    a1 ^% $2 = $0 ->
-    a2 = a1 ->
-    read_half (write_half m a1 v) a2 = v.
-Proof.
-  intros.
-  destruct m as [m P]. unfold ListMemoryNatAddr.mem_size in *.
-  apply ListMemoryNatAddr.write_read_half_eq.
-  - simpl.
-    destruct m; simpl in *; [omega|]. unfold ListMemoryNatAddr.mem_size.
-    apply le_lt_n_Sm.
-    unfold max_addr, ListMemoryNatAddr.mem_size in H.
-    simpl in H.
-    (* if a1=max_addr, the ^+ $1 in H overflows and the lemma doesn't hold,
-       unless we add and use mod hypotheses *)
-    unfold wmod in H0.
-Abort.
-
-Definition valid_addr{w}(m: mem)(alignment: nat)(addr: word w): Prop :=
-  wordToNat addr + alignment <= length (proj1_sig m) /\
-  (wordToNat addr) mod alignment = 0.
 
 Lemma write_read_half_eq: forall w m (a1 a2: word w) v,
     valid_addr m 2 a1 ->
     a2 = a1 ->
     read_half (write_half m a1 v) a2 = v.
 Proof.
-  intros. destruct H.
-  destruct m as [m P]. unfold ListMemoryNatAddr.mem_size in *. simpl in H.
-  apply ListMemoryNatAddr.write_read_half_eq.
-  - simpl.
-    unfold ListMemoryNatAddr.mem_size. omega.
-  - f_equal. assumption.
-Qed.  
+  intros. destruct H, H0.
+  unfold mem_size, ListMemoryNatAddr.mem_size in *.
+  apply ListMemoryNatAddr.write_read_half_eq; try reflexivity.
+  unfold ListMemoryNatAddr.mem_size.
+  omega.
+Qed.
 
 Lemma write_read_half_ne: forall w m (a1 a2: word w) v,
     valid_addr m 2 a1 ->
@@ -219,7 +122,7 @@ Lemma write_read_half_ne: forall w m (a1 a2: word w) v,
     read_half (write_half m a1 v) a2 = read_half m a2.
 Proof.
   intros. destruct H. destruct H0.
-  destruct m as [m P]. unfold ListMemoryNatAddr.mem_size in *. simpl in H, H0.
+  unfold mem_size, ListMemoryNatAddr.mem_size in *. simpl in H, H0.
   apply ListMemoryNatAddr.write_read_half_ne.
   - simpl.
     unfold ListMemoryNatAddr.mem_size. omega.
