0c86140c0b711d9b3386dcffa70129db3259fa0c
Samuel Gruetter <samuel.gruetter@epfl.ch>
2018-05-21 19:40:32 -0400

    wip on loadWord_storeDouble_ne (does not compile)

--- src/Memory.v
+++ src/Memory.v
@@ -18,7 +18,10 @@ Class Memory(m: Set)(w: nat) := mkMemory {
 
   memSize_bound: forall m,
     memSize m <= 2^w;
-        
+
+  memSize_mod8: forall m,
+    memSize m mod 8 = 0;
+  
   loadStoreByte_eq: forall m (a1 a2: word w) v,
     valid_addr a1 1 (memSize m) ->
     a2 = a1 ->
@@ -95,12 +98,117 @@ Class Memory(m: Set)(w: nat) := mkMemory {
      which the store was done. *)
 }.
 
+Lemma mod2_cases: forall (n: nat), n mod 2 = 0 \/ n mod 2 = 1.
+Proof.
+  intros.
+  assert (n mod 2 < 2). {
+    apply Nat.mod_upper_bound. congruence.
+  }
+  omega.
+Qed.
+
+Lemma div_mul_undo: forall a b,
+    b <> 0 ->
+    a mod b = 0 ->
+    a / b * b = a.
+Proof.
+  intros.
+  pose proof Nat.div_mul_cancel_l as A. specialize (A a 1 b).
+  replace (b * 1) with b in A by omega.
+  rewrite Nat.div_1_r in A.
+  rewrite mult_comm.
+  rewrite <- Nat.divide_div_mul_exact; try assumption.
+  - apply A; congruence.
+  - apply Nat.mod_divide; assumption.
+Qed.
+
+Ltac demod :=  
+  repeat match goal with
+         | H: _ mod _ = 0 |- _ => apply Nat.mod_divides in H; [destruct H | congruence]
+         end.
+
 Section MemoryHelpers.
 
   Context {sz: nat}.
   Context {Mem: Set}.
   Context {MM: Memory Mem sz}.
 
+  Lemma loadWord_storeDouble_ne: forall m (a1 a2: word sz) v,
+      valid_addr a1 8 (memSize m) ->
+      valid_addr a2 4 (memSize m) ->
+      a2 <> a1 ->
+      loadWord (storeDouble m a1 v) a2 = loadWord m a2.
+  Proof.
+    intros.
+    pose proof (memSize_bound m) as B.
+    pose proof loadStoreDouble_ne as P.
+    specialize P with (1 := H).
+    destruct (mod2_cases (#a2 / 4)) as [C | C].
+    - specialize (P a2 v).
+      assert (valid_addr a2 8 (memSize m)) as V. {
+        unfold valid_addr in *.
+        assert (#a2 mod 8 = 0). {
+          apply Nat.mod_divide; try congruence.
+          change 8 with (2 * 4).
+          replace #a2 with (#a2 / 4 * 4).
+          - apply Nat.mul_divide_mono_r.
+            apply Nat.mod_divide; try congruence.
+          - apply div_mul_undo; intuition.
+        }
+        intuition.
+        pose proof (memSize_mod8 m).
+        demod. omega.
+      }
+      specialize (P V).
+      rewrite loadDouble_spec in P
+        by (rewrite storeDouble_preserves_memSize; assumption).
+      specialize (P H1).
+      rewrite loadDouble_spec in P by assumption.
+      pose proof combine_inj as Q.
+      specialize (Q 32 32 _ _ _ _ P).
+      destruct Q as [Q _]. assumption.
+    - specialize (P (a2 ^- $4) v).
+      assert (#a2 = 0 \/ #a2 = 1 \/ #a2 = 2 \/ #a2 = 3 \/ 4 <= #a2) as D by omega.
+      destruct D as [D | [D | [D | [D | D]]]];
+        [ (rewrite D in C; simpl in C; discriminate) .. | ].
+      pose proof (loadDouble_spec              m       (a2 ^- $ (4))) as Sp1.
+      pose proof (loadDouble_spec (storeDouble m a1 v) (a2 ^- $ (4))) as Sp2.
+      unfold valid_addr in *.
+      destruct H, H0.
+      rewrite storeDouble_preserves_memSize in Sp2.
+      replace (# (a2 ^- $4)) with (#a2 - 4) in *.
+      + assert ((#a2 - 4) mod 8 = 0) as X. {
+          clear Sp1 Sp2.
+          apply Nat.mod_divide; try congruence.
+          change 8 with (2 * 4).
+          replace (#a2 - 4) with ((#a2 - 4) / 4 * 4).
+          - apply Nat.mul_divide_mono_r.
+            apply Nat.mod_divide; try congruence.
+            admit.
+          - apply div_mul_undo. admit.
+            admit.
+        }
+        assert (#a2 - 4 + 8 <= memSize m) as A. {
+          admit.
+        }
+        specialize (P (conj A X)).
+        rewrite Sp1 in P by (split; assumption). clear Sp1.
+        rewrite Sp2 in P by (split; assumption). clear Sp2.
+        assert (a2 ^- $4 <> a1) as Ne. {
+          admit.
+        }
+        specialize (P Ne).
+        pose proof combine_inj as Q.
+        specialize (Q 32 32 _ _ _ _ P).
+        destruct Q as [_ Q].
+        replace (a2 ^- $ (4) ^+ $ (4)) with a2 in Q by admit.
+        assumption.
+      + rewrite wminus_minus.
+        * rewrite wordToNat_natToWord_idempotent'; [reflexivity|omega].
+        * apply wordToNat_le2.
+          rewrite wordToNat_natToWord_idempotent'; omega.
+  Qed.
+  
   Fixpoint store_byte_list(l: list (word 8))(a: word sz)(m: Mem): Mem :=
     match l with
     | nil => m
