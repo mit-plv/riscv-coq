df7cde6c6b319387865afe911dfc92eb21e70941
Samuel Gruetter <samuel.gruetter@epfl.ch>
2018-05-30 13:28:15 -0400

    apply patch by Andres

--- src/proofs/DecodeEncode.v
+++ src/proofs/DecodeEncode.v
@@ -420,67 +420,353 @@ Goal forall b1 b2, b1 = true -> b2 = true -> true && b1 && (b2 && true) = true.
   intros. prove_andb_true.
 Qed.
 
-Ltac invert_encode :=
-  match goal with
-  | V: context[verify_Invalid   ], H: _ |- _ => pose proof (invert_encode_InvalidInstruction V _ H)
-  | V: context[verify_R         ], H: _ |- _ => pose proof (invert_encode_R                  V _ H)
-  | V: context[verify_R_atomic  ], H: _ |- _ => pose proof (invert_encode_R_atomic           V _ H)
-  | V: context[verify_I         ], H: _ |- _ => pose proof (invert_encode_I                  V _ H)
-  | V: context[verify_I_shift_57], H: _ |- _ => pose proof (invert_encode_I_shift_57         V _ H)
-  | V: context[verify_I_shift_66], H: _ |- _ => pose proof (invert_encode_I_shift_66         V _ H)
-  | V: context[verify_I_system  ], H: _ |- _ => pose proof (invert_encode_I_system           V _ H)
-  | V: context[verify_S         ], H: _ |- _ => pose proof (invert_encode_S                  V _ H)
-  | V: context[verify_SB        ], H: _ |- _ => pose proof (invert_encode_SB                 V _ H)
-  | V: context[verify_U         ], H: _ |- _ => pose proof (invert_encode_U                  V _ H)
-  | V: context[verify_UJ        ], H: _ |- _ => pose proof (invert_encode_UJ                 V _ H)
-  | V: context[verify_Fence     ], H: _ |- _ => pose proof (invert_encode_Fence              V _ H)
-  end.
+Ltac cbn_encode := repeat (
+    cbn [
+      Z.eqb Pos.eqb andb
+opcode_SYSTEM
+opcode_STORE_FP
+opcode_STORE
+opcode_OP_IMM_32
+opcode_OP_IMM
+opcode_OP_FP
+opcode_OP_32
+opcode_OP
+opcode_NMSUB
+opcode_NMADD
+opcode_MSUB
+opcode_MISC_MEM
+opcode_MADD
+opcode_LUI
+opcode_LOAD_FP
+opcode_LOAD
+opcode_JALR
+opcode_JAL
+opcode_BRANCH
+opcode_AUIPC
+opcode_AMO
+funct3_JALR
+funct7_XOR
+funct7_SUBW
+funct7_SRLIW
+funct7_SRL
+funct7_SUB
+funct7_SRLW
+funct7_SRA
+funct7_SLTU
+funct7_SLT
+funct7_SLLW
+funct7_SLLIW
+funct7_SLL
+funct7_SRAW
+funct7_SRAIW
+funct7_MUL
+funct7_DIVW
+funct7_DIVUW
+funct7_DIVU
+funct7_DIV
+funct7_AND
+funct7_SFENCE_VMA
+funct7_REMW
+funct7_REMUW
+funct7_REMU
+funct7_REM
+funct7_OR
+funct7_MULW
+funct7_MULHU
+funct7_MULHSU
+funct7_MULH
+funct3_SRAIW
+funct3_SRAI
+funct3_SRA
+funct3_SLTU
+funct3_SLTIU
+funct3_SLTI
+funct7_ADDW
+funct7_ADD
+funct6_SRLI
+funct6_SRAI
+funct6_SLLI
+funct5_SC
+funct5_LR
+funct5_AMOXOR
+funct5_AMOSWAP
+funct5_AMOOR
+funct5_AMOMINU
+funct5_AMOMIN
+funct5_AMOMAXU
+funct5_AMOMAX
+funct5_AMOAND
+funct5_AMOADD
+funct3_XORI
+funct3_XOR
+funct3_SW
+funct3_SUBW
+funct3_SUB
+funct3_SRLW
+funct3_SRLIW
+funct3_SRLI
+funct3_SRL
+funct3_SRAW
+funct12_EBREAK
+funct3_DIVUW
+funct3_SLT
+funct3_SLLW
+funct3_SLLIW
+funct3_SLLI
+funct3_SLL
+funct3_SH
+funct3_SD
+funct3_SB
+funct3_REMW
+funct3_REMUW
+funct3_REMU
+funct3_REM
+funct3_PRIV
+funct3_ORI
+funct3_OR
+funct3_MULW
+funct3_MULHU
+funct3_MULHSU
+funct3_MULH
+funct3_MUL
+funct3_LWU
+funct3_LW
+funct3_LHU
+funct3_LH
+funct3_LD
+funct3_LBU
+funct3_LB
+funct3_FENCE_I
+funct3_FENCE
+funct3_DIVW
+funct3_AND
+funct3_DIVU
+funct3_DIV
+funct3_CSRRWI
+funct3_CSRRW
+funct3_CSRRSI
+funct3_CSRRS
+funct3_CSRRCI
+funct3_CSRRC
+funct3_BNE
+funct3_BLTU
+funct3_BLT
+funct3_BGEU
+funct3_BGE
+funct3_BEQ
+funct3_ANDI
+funct12_URET
+funct3_AMOW
+funct3_AMOD
+funct3_ADDW
+funct3_ADDIW
+funct3_ADDI
+funct3_ADD
+funct12_WFI
+funct12_MRET
+funct12_SRET
+funct12_ECALL
+isValidM64
+isValidM
+isValidI64
+isValidI
+isValidCSR
+isValidA64
+isValidA
+supportsM
+supportsA
+bitwidth
+app
+] in *;
+cbv [machineIntToShamt id] in *).
 
 
-Lemma pull_let: forall {A B: Type} (a: A) (b: A -> B) (c: B),
-  let x := a in (b x = c) ->
-  (let x := a in b x) = c.
-Proof. intros. simpl. subst x. assumption. Qed.
-
-Lemma decode_encode: forall (inst: Instruction) (z: Z),
-    respects_bounds 64 inst ->
-    encode inst = z ->
-    decode RV64IMA z = inst.
+Lemma decode_encode (inst: Instruction) (z: Z) (H:respects_bounds 64 inst) :
+    decode RV64IMA (encode inst) = inst.
 Proof.
-  intros.
-  let d := eval cbv delta [decode] in decode in change decode with d.
-  cbv beta.
+  cbv beta delta [decode].
   repeat match goal with
-  | |- (let x := ?a in ?b) = ?c => cut (let x := a in (b = c));
-       [apply (pull_let a (fun x => b) c) | intro]
+  | |- (let x := ?a in ?b) = ?c => change (let x := a in b = c); intro
+  | x := ?t : ?T |- _ => pose proof (eq_refl t : x = t); clearbody x
   end.
-  repeat match goal with
-  | x := ?t : ?T |- _ => assert (x = t) by (subst x; reflexivity); clearbody x
-  end.
-  unfold encode in *. repeat autounfold with mappers in *.
-  destruct inst;  [destruct i..|subst; exfalso; assumption];
-  try reflexivity;
-  try (time (
-  simpl in H;
-  invert_encode;
-  try (exfalso; assumption);
-  repeat match goal with
-         | H: _ /\ _ |- _ =>
-           let E := fresh "E" in
-           destruct H as [E H];
-           rewrite <- E in *;
-           clear E;
-           try (rewrite <- H in *; clear H)
-  end;
-  subst;
-  repeat lazymatch goal with
-         | |- context [if (?x && ?y) then ?a else ?b] =>
-           (replace (x && y) with false by (symmetry; prove_andb_false)) ||
-           (replace (x && y) with true  by (symmetry; prove_andb_true))
-         end;
-  (reflexivity || (idtac "Failed to solve:"; match goal with
-                   | |- ?G => idtac G
-                   end))
-  )).
-  all: solve [exfalso; assumption].
-Time Qed.
+  remember (encode inst) as encoded eqn:Henc; symmetry in Henc.
+  cbv [encode] in Henc.
+  cbv [
+      Encoder
+        Verifier
+        apply_InstructionMapper 
+        map_Fence
+        map_I
+        map_I_shift_57
+        map_I_shift_66
+        map_I_system
+        map_Invalid
+        map_R
+        map_R_atomic
+        map_S
+        map_SB
+        map_U
+        map_UJ
+    ] in Henc.
+
+  destruct inst as [i|i|i|i|i|i|i|i].
+  (* TODO: use par:abstract here once all of the proof is automated into the next sentence *)
+  all: destruct i; try (
+    (lazymatch type of Henc with
+     | encode_I _ _ _ _ _ = _ =>
+       apply invert_encode_I in Henc
+     | encode_Fence _ _ _ _ _ _ _ = _ =>
+       apply invert_encode_Fence in Henc
+     | encode_I_shift_66 _ _ _ _ _ _ = _ =>
+       apply (invert_encode_I_shift_66(bitwidth:=64)) in Henc
+     | encode_I_shift_57 _ _ _ _ _ _ = _ =>
+       apply invert_encode_I_shift_57 in Henc
+     | encode_R _ _ _ _ _ _ = _ =>
+       apply invert_encode_R in Henc
+     | encode_Invalid _ = _ =>
+       apply invert_encode_InvalidInstruction in Henc
+     | encode_R_atomic _ _ _ _ _ _ _ = _ => 
+       apply invert_encode_R_atomic in Henc
+     | encode_I_system _ _ _ _ _ = _ =>
+       apply invert_encode_I_system in Henc
+     | encode_U _ _ _ = _ =>
+       apply invert_encode_U in Henc
+     | encode_UJ _ _ _ = _ =>
+       apply invert_encode_UJ in Henc
+     | encode_S _ _ _ _ _ = _ =>
+       apply invert_encode_S in Henc
+     | encode_SB _ _ _ _ _ = _ => 
+       apply invert_encode_SB in Henc
+     end; [|trivial]);
+      repeat match type of Henc with
+               _ /\ _ => let H := fresh "H" in destruct Henc as [H Henc]; rewrite <-?H in *
+             end; rewrite <-?Henc in *;
+      subst results; subst resultI; subst decodeI; subst opcode; subst funct3;
+      subst funct5; subst funct6; subst funct7; subst funct10; subst funct12;
+      repeat match goal with
+      | H: False |- _ => destruct H
+      | |- ?x = ?x => exact_no_check (eq_refl x)
+      | |- _ => progress cbn_encode
+      | |- _ => rewrite !Bool.orb_true_r in *
+      | |- _ => rewrite !Bool.andb_false_r in *
+      | |- _ => progress subst
+      end).
+
+  (* cases where bitSlice in goal and hyps do not match (but the encoded value is fully specified) *)
+  all : cbv [funct7_SFENCE_VMA opcode_SYSTEM funct3_PRIV funct12_WFI funct12_MRET funct12_SRET funct12_URET funct12_EBREAK funct12_ECALL funct3_FENCE_I opcode_MISC_MEM isValidI] in *.
+  all:
+    repeat match goal with
+           | |- ?x = ?x => exact_no_check (eq_refl x)
+           | |- context [?x =? ?y] =>
+             let H := fresh "H" in
+             destruct (x =? y) eqn:H;
+               apply Z.eqb_eq in H || apply Z.eqb_neq in H
+           | _ => progress cbn in *
+           end.
+  all : exfalso; try match goal with H: _ <> _ |- _ => apply H; clear H end.
+  (* 7 subgoals (ID 91796) *)
+  
+(*   z : Z *)
+(*   H : verify_Fence opcode_MISC_MEM 0 0 funct3_FENCE_I 0 0 0 *)
+(*   encoded : MachineInt *)
+(*   H43 : 15 = bitSlice encoded 0 7 *)
+(*   H44 : 1 = bitSlice encoded 12 15 *)
+(*   H45 : 0 = bitSlice encoded 7 12 *)
+(*   H46 : 0 = bitSlice encoded 15 20 *)
+(*   H47 : 0 = bitSlice encoded 20 24 *)
+(*   H48 : 0 = bitSlice encoded 24 28 *)
+(*   Henc : 0 = bitSlice encoded 28 32 *)
+(*   ============================ *)
+(*   signExtend 12 (bitSlice encoded 20 32) = 0 *)
+
+  admit.
+(* 6 subgoals (ID 91789) *)
+  
+(*   z : Z *)
+(*   H : verify_I_system opcode_SYSTEM 0 0 funct3_PRIV funct12_ECALL *)
+(*   encoded : MachineInt *)
+(*   H43 : 115 = bitSlice encoded 0 7 *)
+(*   H44 : 0 = bitSlice encoded 12 15 *)
+(*   H45 : 0 = bitSlice encoded 20 32 *)
+(*   H46 : 0 = bitSlice encoded 7 12 *)
+(*   Henc : 0 = bitSlice encoded 15 20 *)
+(*   H0 : bitSlice encoded 25 32 = 9 *)
+(*   ============================ *)
+(*   False *)
+
+  admit.
+
+ (* 5 subgoals (ID 91790) *)
+  
+(*   z : Z *)
+(*   H : verify_I_system opcode_SYSTEM 0 0 funct3_PRIV funct12_EBREAK *)
+(*   encoded : MachineInt *)
+(*   H43 : 115 = bitSlice encoded 0 7 *)
+(*   H44 : 0 = bitSlice encoded 12 15 *)
+(*   H45 : 1 = bitSlice encoded 20 32 *)
+(*   H46 : 0 = bitSlice encoded 7 12 *)
+(*   Henc : 0 = bitSlice encoded 15 20 *)
+(*   H0 : bitSlice encoded 25 32 = 9 *)
+(*   ============================ *)
+(*   False *)
+  
+  admit.
+(* 4 subgoals (ID 91791) *)
+  
+(*   z : Z *)
+(*   H : verify_I_system opcode_SYSTEM 0 0 funct3_PRIV funct12_URET *)
+(*   encoded : MachineInt *)
+(*   H43 : 115 = bitSlice encoded 0 7 *)
+(*   H44 : 0 = bitSlice encoded 12 15 *)
+(*   H45 : 2 = bitSlice encoded 20 32 *)
+(*   H46 : 0 = bitSlice encoded 7 12 *)
+(*   Henc : 0 = bitSlice encoded 15 20 *)
+(*   H0 : bitSlice encoded 25 32 = 9 *)
+(*   ============================ *)
+(*   False *)
+
+  admit.
+  (* 3 subgoals (ID 91792) *)
+  
+(*   z : Z *)
+(*   H : verify_I_system opcode_SYSTEM 0 0 funct3_PRIV funct12_SRET *)
+(*   encoded : MachineInt *)
+(*   H43 : 115 = bitSlice encoded 0 7 *)
+(*   H44 : 0 = bitSlice encoded 12 15 *)
+(*   H45 : 258 = bitSlice encoded 20 32 *)
+(*   H46 : 0 = bitSlice encoded 7 12 *)
+(*   Henc : 0 = bitSlice encoded 15 20 *)
+(*   H0 : bitSlice encoded 25 32 = 9 *)
+(*   ============================ *)
+(*   False *)
+  admit.
+  (* 2 subgoals (ID 91793) *)
+  
+(*   z : Z *)
+(*   H : verify_I_system opcode_SYSTEM 0 0 funct3_PRIV funct12_MRET *)
+(*   encoded : MachineInt *)
+(*   H43 : 115 = bitSlice encoded 0 7 *)
+(*   H44 : 0 = bitSlice encoded 12 15 *)
+(*   H45 : 770 = bitSlice encoded 20 32 *)
+(*   H46 : 0 = bitSlice encoded 7 12 *)
+(*   Henc : 0 = bitSlice encoded 15 20 *)
+(*   H0 : bitSlice encoded 25 32 = 9 *)
+(*   ============================ *)
+(*   False *)
+  admit.
+  (* 1 subgoal (ID 91794) *)
+  
+  (* z : Z *)
+  (* H : verify_I_system opcode_SYSTEM 0 0 funct3_PRIV funct12_WFI *)
+  (* encoded : MachineInt *)
+  (* H43 : 115 = bitSlice encoded 0 7 *)
+  (* H44 : 0 = bitSlice encoded 12 15 *)
+  (* H45 : 261 = bitSlice encoded 20 32 *)
+  (* H46 : 0 = bitSlice encoded 7 12 *)
+  (* Henc : 0 = bitSlice encoded 15 20 *)
+  (* H0 : bitSlice encoded 25 32 = 9 *)
+  (* ============================ *)
+  (* False *)
+  admit.
+
+Admitted.
