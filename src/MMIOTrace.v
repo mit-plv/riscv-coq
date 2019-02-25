3b26d6f856a1c13a391be5f47f5011d589abf08f
Samuel Gruetter <samuel.gruetter@epfl.ch>
2019-02-25 16:01:07 -0500

    alias MMIOAction to bool so that bedrock2/bedrock2 can use the same action name without depending on riscv-coq

--- src/MMIOTrace.v
+++ src/MMIOTrace.v
@@ -1,1 +1,15 @@
+(*
+We do not do this:
+
 Inductive MMIOAction := MMInput | MMOutput.
+
+Because other libraries (such as bedrock2/bedrock2) don't depend on riscv-coq, but should
+still be able to use the same data type in their trace.
+*)
+
+Definition MMIOAction: Type := bool.
+Definition MMInput: MMIOAction := false.
+Definition MMOutput: MMIOAction := true.
+
+Definition isMMInput(a: MMIOAction): bool := negb a.
+Definition isMMOutput(a: MMIOAction): bool := a.
