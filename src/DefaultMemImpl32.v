1014999aef8fbc75a3b3822b49c818bbac586d37
Samuel Gruetter <samuel.gruetter@epfl.ch>
2019-02-26 18:19:37 -0500

    use the right SortedList params

--- src/DefaultMemImpl32.v
+++ src/DefaultMemImpl32.v
@@ -3,7 +3,8 @@ Require Import Coq.micromega.Lia.
 Require Import coqutil.Word.Interface coqutil.Word.Properties coqutil.Word.Naive.
 Require Import coqutil.Map.Interface.
 Require Import riscv.Words32Naive.
-Require coqutil.Map.SortedList.
+Require coqutil.Map.SortedList coqutil.Map.SortedListWord.
+
 
 Instance params: SortedList.parameters := {|
   SortedList.parameters.key := word32;
@@ -11,15 +12,5 @@ Instance params: SortedList.parameters := {|
   SortedList.parameters.ltb := word.ltu;
 |}.
 
-Instance strictorder: SortedList.parameters.strict_order SortedList.parameters.ltb.
-constructor; simpl; intros; rewrite? Z.ltb_nlt in *; rewrite? Z.ltb_lt in *; try lia.
-apply (@word.unsigned_inj 32 word32 word32ok). (* TODO can we make this work without @ ? *)
-(* TODO how can we normalize this so that lia will recognize the terms as being the same? *)
-match goal with
-| H: ~ ?x < ?y |- ?x' = ?y' => change x' with x; change y' with y
-end.
-lia.
-Qed.
-
-Instance Mem: map.map word32 word8 := SortedList.map params strictorder.
+Instance Mem: map.map word32 word8 := SortedList.map params _.
 Instance MemOk: map.ok Mem := SortedList.map_ok.
