c96a7b63a5fa0cec3bd50905e3e79bb70044f6f1
Samuel Gruetter <samuel.gruetter@epfl.ch>
2020-11-21 00:04:26 -0500

    in some contexts, the parent folder might not be named deps

--- src/riscv/Examples/SMTVerif.sh
+++ src/riscv/Examples/SMTVerif.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-coqc -R .. riscv -Q ../../../../../deps/coqutil/src/coqutil/ coqutil ./SMTVerif.v | z3 -in
+coqc -R .. riscv -Q ../../../../coqutil/src/coqutil/ coqutil ./SMTVerif.v | z3 -in
