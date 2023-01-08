d439d53089c4773d5d9c9f423d77202dc29c216b
Samuel Gruetter <saemi472@gmail.com>
2023-01-07 21:39:07 -0500

    move record eta expansion to coqutil

--- src/riscv/Utility/RecordSettersUsingExistingGetters.v
+++ src/riscv/Utility/RecordSettersUsingExistingGetters.v
@@ -1,56 +1,9 @@
 Require Import Coq.Program.Basics.
 Require Import Ltac2.Ltac2.
-Require Ltac2.Option.
 Require Import Ltac2.Bool.
 Require Import coqutil.Ltac2Lib.Constr.
+Require Import coqutil.Tactics.RecordEta.
 Set Default Proof Mode "Classic".
-
-Ltac2 rec strip_foralls(t: constr) :=
-  match Constr.Unsafe.kind t with
-  | Constr.Unsafe.Prod b u => let (bs, body) := strip_foralls u in (b :: bs, body)
-  | _ => ([], t)
-  end.
-
-Ltac2 app_arg_count(t: constr) :=
-  match Constr.Unsafe.kind t with
-  | Constr.Unsafe.App f args => Array.length args
-  | _ => 0
-  end.
-
-Ltac2 binder_to_field(qualification: ident list)(b: binder) :=
-   Option.get (Env.get (List.append qualification [Option.get (Constr.Binder.name b)])).
-
-Ltac2 field_names(ctor_ref: Std.reference) :=
-  let ctor_type := Constr.type (Env.instantiate ctor_ref) in
-  let (binders, result) := strip_foralls ctor_type in
-  let n_type_args := app_arg_count result in
-  let field_name_binders := List.skipn n_type_args binders in
-  List.map (binder_to_field (List.removelast (Env.path ctor_ref))) field_name_binders.
-
-Ltac2 constructor_of_record(t: constr) :=
-  match Constr.Unsafe.kind t with
-  | Constr.Unsafe.Ind ind inst =>
-    Std.ConstructRef (Constr.Unsafe.constructor ind 0)
-  | _ => Control.throw (Invalid_argument (Some (Message.of_constr t)))
-  end.
-
-Ltac2 eta(t: constr) :=
-  let (h, args) := match Constr.Unsafe.kind t with
-                   | Constr.Unsafe.App h args => (h, args)
-                   | _ => (t, Array.empty ())
-                   end in
-  let ctor := constructor_of_record h in
-  let getters := List.map (fun getterRef => mkApp (Env.instantiate getterRef) args)
-                          (field_names ctor) in
-  constr:(fun x: $t => ltac2:(
-    let projections := List.map (fun getter => constr:($getter &x)) getters in
-    let res := mkApp (mkApp (Env.instantiate ctor) args) (Array.of_list projections) in
-    exact $res)).
-
-Ltac exact_eta :=
-  ltac2:(t |- let res := eta (Option.get (Ltac1.to_constr t)) in exact $res).
-
-Ltac eta T := constr:(ltac:(exact_eta T)).
 
 Class Setter{R E: Type}(getter: R -> E): Type := set: (E -> E) -> R -> R.
 Arguments set {R E} (getter) {Setter} (fieldUpdater) (r).
