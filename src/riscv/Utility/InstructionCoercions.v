55c9cc88a6734550bdd913bfdf482d498c6354e6
Samuel Gruetter <saemi472@gmail.com>
2023-03-16 18:36:44 -0400

    print register names like t0, t1, a0, s0, etc instead of x0, x1, etc

--- src/riscv/Utility/InstructionCoercions.v
+++ src/riscv/Utility/InstructionCoercions.v
@@ -1,10 +1,14 @@
 Require Import riscv.Spec.Decode.
 
-Coercion IInstruction: InstructionI >-> Instruction.
-Coercion MInstruction: InstructionM >-> Instruction.
-Coercion I64Instruction: InstructionI64 >-> Instruction.
-Coercion M64Instruction: InstructionM64 >-> Instruction.
-Coercion CSRInstruction: InstructionCSR >-> Instruction.
+Coercion IInstruction   : InstructionI   >-> Instruction.
+Coercion MInstruction   : InstructionM   >-> Instruction.
+Coercion AInstruction   : InstructionA   >-> Instruction.
+Coercion FInstruction   : InstructionF   >-> Instruction.
+Coercion I64Instruction : InstructionI64 >-> Instruction.
+Coercion M64Instruction : InstructionM64 >-> Instruction.
+Coercion A64Instruction : InstructionA64 >-> Instruction.
+Coercion F64Instruction : InstructionF64 >-> Instruction.
+Coercion CSRInstruction : InstructionCSR >-> Instruction.
 
 (* separate notation to make sure coercions kick in *)
 Declare Scope ilist_scope.
---
+++ src/riscv/Utility/RegisterNameNotations.v
@@ -0,0 +1,35 @@
+Require Import Coq.ZArith.BinInt.
+Declare Custom Entry register_name.
+
+Notation "'zero'" :=   0%Z (in custom register_name).
+Notation "'ra'"   :=   1%Z (in custom register_name).
+Notation "'sp'"   :=   2%Z (in custom register_name).
+Notation "'gp'"   :=   3%Z (in custom register_name).
+Notation "'tp'"   :=   4%Z (in custom register_name).
+Notation "'t0'"   :=   5%Z (in custom register_name).
+Notation "'t1'"   :=   6%Z (in custom register_name).
+Notation "'t2'"   :=   7%Z (in custom register_name).
+Notation "'s0'"   :=   8%Z (in custom register_name).
+Notation "'s1'"   :=   9%Z (in custom register_name).
+Notation "'a0'"   :=  10%Z (in custom register_name).
+Notation "'a1'"   :=  11%Z (in custom register_name).
+Notation "'a2'"   :=  12%Z (in custom register_name).
+Notation "'a3'"   :=  13%Z (in custom register_name).
+Notation "'a4'"   :=  14%Z (in custom register_name).
+Notation "'a5'"   :=  15%Z (in custom register_name).
+Notation "'a6'"   :=  16%Z (in custom register_name).
+Notation "'a7'"   :=  17%Z (in custom register_name).
+Notation "'s2'"   :=  18%Z (in custom register_name).
+Notation "'s3'"   :=  19%Z (in custom register_name).
+Notation "'s4'"   :=  20%Z (in custom register_name).
+Notation "'s5'"   :=  21%Z (in custom register_name).
+Notation "'s6'"   :=  22%Z (in custom register_name).
+Notation "'s7'"   :=  23%Z (in custom register_name).
+Notation "'s8'"   :=  24%Z (in custom register_name).
+Notation "'s9'"   :=  25%Z (in custom register_name).
+Notation "'s10'"  :=  26%Z (in custom register_name).
+Notation "'s11'"  :=  27%Z (in custom register_name).
+Notation "'t3'"   :=  28%Z (in custom register_name).
+Notation "'t4'"   :=  29%Z (in custom register_name).
+Notation "'t5'"   :=  30%Z (in custom register_name).
+Notation "'t6'"   :=  31%Z (in custom register_name).
