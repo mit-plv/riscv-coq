d7c69b0f9092c5127dfffb72984232e7f57f5135
Andres Erbsen <andres@andres.systems>
2025-09-05 03:06:37 -0400

    merge composed build

--- src/riscv/BUILD.mk
+++ src/riscv/BUILD.mk
@@ -1,6 +1,6 @@
 RISCV_DIR := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
 RISCV_VFILES := $(call rwildcard,$(RISCV_DIR),*.v)
-RISCV_COQDEPFLAGS := -Q $(RISCV_DIR) $(notdir $(RISCV_DIR))
+RISCV_COQDEPFLAGS := -R $(RISCV_DIR) $(notdir $(RISCV_DIR))
 RISCV_REQUIREFLAGS := -Q $(O)/$(RISCV_DIR) $(notdir $(RISCV_DIR))
 
 RISCV_COQFLAGS := $(COQUTIL_REQUIREFLAGS) -R $(O)/$(RISCV_DIR) $(notdir $(RISCV_DIR)) -w -deprecated-from-Coq,-deprecated-since-9.0,-deprecated-since-8.20
