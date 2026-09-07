c8c35608449acbc37c76d8b032486c4751e21cc9
Lukas Zobernig <163596910+lukaszobernig@users.noreply.github.com>
2026-09-07 14:41:58 +0000

    Fix Words32Naive typeclass instances for Rocq 9.0

--- src/riscv/Utility/Words32Naive.v
+++ src/riscv/Utility/Words32Naive.v
@@ -2,13 +2,13 @@ Require Import Coq.ZArith.BinInt.
 Require Import coqutil.Z.Lia.
 Require Import coqutil.Word.Properties.
 Require Import coqutil.Word.Bitwidth.
-Require Export coqutil.Word.Naive.
+Require Import coqutil.Word.Naive.
 
 Local Open Scope Z_scope.
 
-#[export] Hint Extern 0 (word.word 32) => exact (Naive.word 32) : typeclass_instances.
-Notation word := (Naive.word 32).
+#[global] Instance word: word.word 32 := Naive.word 32.
+#[global] Instance word_ok: word.ok word := Naive.word32_ok.
 
-#[export] Instance Words32Naive: Bitwidth 32 := {|
+#[global] Instance Words32Naive: Bitwidth 32 := {|
   width_cases := or_introl eq_refl;
 |}.
